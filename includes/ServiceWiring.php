e15e3347cc52ad48ee69562eb3c15faa3dcf6c6d
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2021-05-12 02:05:15 +0000

    build: Updating dependencies
    composer:
    * mediawiki/mediawiki-codesniffer: 35.0.0 → 36.0.0
    * php-parallel-lint/php-parallel-lint: 1.2.0 → 1.3.0
    
    npm:
    * grunt: 1.3.0 → 1.4.0
    * lodash: 4.17.20 → 4.17.21
      * https://npmjs.com/advisories/1673 (CVE-2021-23337)
    
    Change-Id: Iee88311dd09e5f32e4014794266c8a611f3b572b

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -4,7 +4,7 @@ use MediaWiki\MediaWikiServices;
 
 return [
 
-	'NotesLinkNDLFactory' => function ( MediaWikiServices $services ) {
+	'NotesLinkNDLFactory' => static function ( MediaWikiServices $services ) {
 		return new \NotesLink\NDLFactory(
 			$services->getConfigFactory()->makeConfig( 'noteslink' )
 		);
